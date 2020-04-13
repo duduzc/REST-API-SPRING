4ccf694ebd3b750a43fe508b15ab0778f24eff33
eduardo <eduardo@192.168.18.13>
2020-04-13 12:58:57 -0300

    Implementation first generic method post and getById.

--- spring-api/src/main/java/com/cavalcante/springapi/resource/generic/GenericResource.java
+++ spring-api/src/main/java/com/cavalcante/springapi/resource/generic/GenericResource.java
@@ -1,10 +1,17 @@
 package com.cavalcante.springapi.resource.generic;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cavalcante.springapi.model.generic.GenericModel;
 import com.cavalcante.springapi.repository.generic.GenericRepository;
@@ -23,5 +30,20 @@ public class GenericResource<T extends GenericModel> {
 	@RequestMapping(method = RequestMethod.GET)
 	public List<T> listAll() {
 		return genericRepository.findAll();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<T> create(@RequestBody T entity, HttpServletResponse response) { 
+		T entitySave = genericRepository.save(entity);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(entitySave.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(entitySave);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public T getById(@PathVariable(value = "id") long id) { 
+		return genericRepository.findById(id).orElse(null);
 	}
 }
