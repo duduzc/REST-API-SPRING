328e80ea01b54631de2f4c370cf7d2afe1475db7
eduardo <eduardo@10.0.0.227>
2020-04-16 19:26:12 -0300

    Implementation BeanValidation and ExceptionHandler.

--- spring-api/src/main/java/com/cavalcante/springapi/resource/generic/GenericResource.java
+++ spring-api/src/main/java/com/cavalcante/springapi/resource/generic/GenericResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -33,7 +34,7 @@ public class GenericResource<T extends GenericModel> {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<T> create(@RequestBody T entity, HttpServletResponse response) { 
+	public ResponseEntity<T> create(@Valid @RequestBody T entity, HttpServletResponse response) { 
 		T entitySave = genericRepository.save(entity);
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
